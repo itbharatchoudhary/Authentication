9571b06ead809a1aba9bbf3b7106aa887553f320
Bharat Choudhary <bharatchoutharyb@gmail.com>
2026-03-25 19:32:01 +0530

    done

--- Frontend/src/Features/Auth/AuthSlice.js
+++
@@ -1,3 +0,0 @@
-// If migrating to Redux Toolkit, place slice here.
-// For now this re-exports the Zustand store for compatibility.
-export { useAuthStore as default } from '../../App/Store';
--- Frontend/src/Features/User/Services/User.api.js
+++
@@ -1,5 +0,0 @@
-import api from '../../../Services/Axios';
-
-export const getMe        = ()  => api.get('/users/me');
-export const getAllUsers   = ()  => api.get('/users');
-export const deleteUser   = (id)=> api.delete(`/users/${id}`);
