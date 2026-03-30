2b376fdb7c0e5914f278a6f4d6ab2b1360adea73
Bharat Choudhary <bharatchoutharyb@gmail.com>
2026-03-30 14:22:35 +0530

    done

--- Frontend/src/Services/Api.js
+++ Frontend/src/Services/Api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "http://localhost:3000/api",
 });
 
-// 🔐 Attach token automatically
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
